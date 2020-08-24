6b61d90ba68da215546c4536765f15c7ae6fc5fd
tnm <ted@ted.io>
2020-08-24 12:01:26 -0700

    Version 0.3.3 release

--- cased/__init__.py
+++ cased/__init__.py
@@ -5,7 +5,7 @@ from cased.data.context import Context
 # =============================================================
 # Package settings
 # =============================================================
-VERSION = "0.3.2"
+VERSION = "0.3.3"
 APP_NAME = "Cased"
 
 
